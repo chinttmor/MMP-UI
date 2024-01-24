02c59ba7198e9c4d0ed474bb3e614cab8cef01aa
chinttmor <chi.ntt@mor.com.vn>
2024-01-24 15:32:45 +0700

    update refreshToken , function works now

--- src/app/hooks/useRefreshToken.ts
+++ src/app/hooks/useRefreshToken.ts
@@ -1,17 +1,18 @@
 // hooks/auth/useRefreshToken.ts
 import instance from 'config/axios.config';
 import { signIn, useSession } from 'next-auth/react';
-
 export const useRefreshToken = () => {
   const { data: session } = useSession();
-
+  console.log('rf', session?.user.refreshToken);
   const refreshToken = async () => {
     // Gọi tới backend để lấy access token mới và trả về
-    const res = await instance.post('/auth/acstoken', {
-      refreshToken: session?.user.refreshToken,
+    const res = await instance.get('auth/acstoken', {
+      headers: {
+        Authorization: `Bearer ${session?.user.refreshToken}`,
+      },
     });
-
-    if (session) session.user.accessToken = res.data.tokens.accessToken;
+    console.log('rf res', res);
+    if (session) session.user.accessToken = res.data.data.accessToken;
     else signIn();
   };
   return refreshToken;

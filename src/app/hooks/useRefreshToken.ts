401e16190b0d5b26dcf649617c3f3de546638e4a
chinttmor <chi.ntt@mor.com.vn>
2024-01-24 15:53:03 +0700

    delete console.log

--- src/app/hooks/useRefreshToken.ts
+++ src/app/hooks/useRefreshToken.ts
@@ -3,7 +3,6 @@ import instance from 'config/axios.config';
 import { signIn, useSession } from 'next-auth/react';
 export const useRefreshToken = () => {
   const { data: session } = useSession();
-  console.log('rf', session?.user.refreshToken);
   const refreshToken = async () => {
     // Gọi tới backend để lấy access token mới và trả về
     const res = await instance.get('auth/acstoken', {
@@ -11,7 +10,6 @@ export const useRefreshToken = () => {
         Authorization: `Bearer ${session?.user.refreshToken}`,
       },
     });
-    console.log('rf res', res);
     if (session) session.user.accessToken = res.data.data.accessToken;
     else signIn();
   };

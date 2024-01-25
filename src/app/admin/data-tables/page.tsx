01da5e74f2f4ee1971ce1e8ef36b9dc5ff57bd8b
chinttmor <chi.ntt@mor.com.vn>
2024-01-25 09:09:58 +0700

    delete unused commnents

--- src/app/admin/data-tables/page.tsx
+++ src/app/admin/data-tables/page.tsx
@@ -17,12 +17,6 @@ const Tables = () => {
   return (
     <div className="mt-10">
       <ColumnsTable tableData={tableDataColumns} />
-      {/* <button
-        className="linear w-full rounded-xl bg-brand-500 bg-cyan-300 py-3 text-base font-medium text-white text-white transition duration-200 hover:bg-brand-600 active:bg-brand-700 dark:bg-brand-400 dark:hover:bg-brand-300 dark:active:bg-brand-200"
-        onClick={onSubmit}
-      >
-        Sign In
-      </button> */}
     </div>
   );
 };
